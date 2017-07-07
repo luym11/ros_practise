7923b05ce46c751651c78c75e513be88cad5fee5
yimeng <lym602@126.com>
2017-07-07 14:21:29 +0300

    init files

---
+++ include/husky_highlevel_controller/HuskyHighlevelController.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <ros/ros.h>
+
+namespace husky_highlevel_controller {
+
+/*!
+ * Class containing the Husky Highlevel Controller
+ */
+class HuskyHighlevelController {
+public:
+	/*!
+	 * Constructor.
+	 */
+	HuskyHighlevelController(ros::NodeHandle& nodeHandle);
+
+	/*!
+	 * Destructor.
+	 */
+	virtual ~HuskyHighlevelController();
+
+private:
+	ros::NodeHandle nodeHandle_;
+};
+
+} /* namespace */
---
+++ src/HuskyHighlevelController.cpp
@@ -0,0 +1,15 @@
+#include "husky_highlevel_controller/HuskyHighlevelController.hpp"
+
+namespace husky_highlevel_controller {
+
+HuskyHighlevelController::HuskyHighlevelController(ros::NodeHandle& nodeHandle) :
+  nodeHandle_(nodeHandle)
+{
+    
+}
+
+HuskyHighlevelController::~HuskyHighlevelController()
+{
+}
+
+} /* namespace */
