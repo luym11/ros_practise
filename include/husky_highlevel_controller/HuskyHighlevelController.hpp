f9572d6c4f9ae9b4d71dafee3a57322bfa3fbb96
yimeng <lym602@126.com>
2017-07-07 21:22:33 +0300

    exercise2: simple simulation environment

--- include/husky_highlevel_controller/HuskyHighlevelController.hpp
+++ include/husky_highlevel_controller/HuskyHighlevelController.hpp
@@ -1,6 +1,13 @@
 #pragma once
 
 #include <ros/ros.h>
+#include <sensor_msgs/LaserScan.h>
+#include <vector>
+#include <string>
+#include <std_msgs/Float32.h>
+
+using std::vector;
+using std::string;
 
 namespace husky_highlevel_controller {
 
@@ -21,6 +28,15 @@ public:
 
 private:
 	ros::NodeHandle nodeHandle_;
+	void laser_scan_Callback( const sensor_msgs::LaserScan::ConstPtr& laser_scan_msgs );
+	vector<float> laser_scan_distance; 
+	string topic_name; 
+	int queue_size;
+	float minDistance; 
+
+	ros::Subscriber laser_scan_subs;
+	ros::Publisher distance_to_husky; 
+ 
 };
 
 } /* namespace */
--- src/HuskyHighlevelController.cpp
+++ src/HuskyHighlevelController.cpp
@@ -5,7 +5,21 @@ namespace husky_highlevel_controller {
 HuskyHighlevelController::HuskyHighlevelController(ros::NodeHandle& nodeHandle) :
   nodeHandle_(nodeHandle)
 {
-    
+	nodeHandle.getParam("/husky_highlevel_controller/husky/topic_name", topic_name); 
+	nodeHandle.getParam("/husky_highlevel_controller/husky/queue_size", queue_size);     
+	ROS_INFO_STREAM(topic_name << " " << queue_size); 
+	laser_scan_subs = nodeHandle.subscribe( topic_name, queue_size, &HuskyHighlevelController::laser_scan_Callback, this); 
+}
+
+void HuskyHighlevelController::laser_scan_Callback( const sensor_msgs::LaserScan::ConstPtr& laser_scan_msgs ){
+	minDistance = 30; 
+	laser_scan_distance = laser_scan_msgs->ranges;
+	ROS_INFO_STREAM("msgs received" << std::endl); 
+	for(int i = 0; i < laser_scan_distance.size(); i++){
+		// ROS_INFO_STREAM("The " << i << "st message in " << laser_scan_distance.size() << " messages");
+		 if(minDistance > laser_scan_distance[i]) minDistance = laser_scan_distance[i]; 
+	}
+	ROS_INFO_STREAM("minDistance detected by Lidar: " << minDistance << " ");
 }
 
 HuskyHighlevelController::~HuskyHighlevelController()
